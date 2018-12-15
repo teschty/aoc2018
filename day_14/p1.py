b9956601a27e9e1ba592b0976b63dbfb32aac01c
Tyler Tesch <tytesch@gmail.com>
2018-12-15 10:03:14 -0600

    Add day 14 part 1

---
+++ day_14/p1.py
@@ -0,0 +1,19 @@
+import time
+
+with open("input.txt") as f:
+    after_amount = int(f.read().strip())
+
+recipes = "37"
+first_elf = 0
+second_elf = 1
+
+while len(recipes) < after_amount + 10:
+    one = int(recipes[first_elf])
+    two = int(recipes[second_elf])
+
+    recipes += str(one + two)
+
+    first_elf = (first_elf + 1 + one) % len(recipes)
+    second_elf = (second_elf + 1 + two) % len(recipes)
+
+print(recipes[after_amount:])
